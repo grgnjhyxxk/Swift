0dd547220ec8c2764673330605851af4d1009f00
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-04-12 00:24:33 +0900

    Logical-Operators

---
+++ 02.Operators/04.Logical-Operators.playground/Contents.swift
@@ -0,0 +1,13 @@
+// 논리 연산자
+
+// NOT(부정) 논리 연산자 (!A) : 불리언 값 B의 값을 반전합니다.
+let trueBoolan: Bool = true
+let falseBoolan: Bool = false
+!trueBoolan // false
+!falseBoolan // true
+
+// AND 논리 연산자 (A && B) : A와 B의 불리언 AND 논리 연산을 실행합니다.
+trueBoolan && falseBoolan // false
+
+// OR 논리 연산자 (A || B) : A와 B의 불리언 OR 논리 연산을 실행합니다.
+trueBoolan || falseBoolan // true
