31c4fb7c44d703925617d1452e99a79f232c5906
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-04-12 00:24:08 +0900

    Ternary-Conditional-Operators

---
+++ 02.Operators/02.Ternary-Conditional-Operators.playground/Contents.swift
@@ -0,0 +1,10 @@
+// 삼항 조건 연산자의 사용
+let a = 10
+let b = 5
+
+var result = a > b ? "a is greater than b" : "b is greater than a"
+print(result) // a is greater than b
+
+let number = 7
+result = number % 2 == 0 ? "even" : "odd"
+print(result) // odd
